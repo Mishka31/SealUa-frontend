f67758a9a39bb5c8f149c4b7bc2fe62a2116ef99
Misha Krasnonos <mikhail.k@kitelytech.com>
2022-10-22 01:52:44 +0300

    start marckup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+console.log('React 🔰', React.version);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
