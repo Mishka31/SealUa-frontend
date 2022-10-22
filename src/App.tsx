1fe611b2188673505c78054ea4aa84038c5ce263
Misha Krasnonos <mikhail.k@kitelytech.com>
2022-10-22 03:24:54 +0300

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ console.log('React 🔰', React.version);
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Misha</h1>
+        <h1>Hello World!</h1>
+        <button>Start</button>
+      </header>
     </div>
   );
 }
