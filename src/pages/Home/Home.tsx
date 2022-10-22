693ac5aca7f1c71830e7f52137821ca031104c36
Misha Krasnonos <mikhail.k@kitelytech.com>
2022-10-22 05:22:01 +0300

    asdads

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,6 +7,17 @@ export const Home = () => {
     window.scrollTo(0, 1);
   }, 1000);
 
+  const doc = window.document;
+  const docEl = doc.documentElement;
+
+  const requestFullScreen = docEl.requestFullscreen;
+  const cancelFullScreen = doc.exitFullscreen;
+
+  if (!doc.fullscreenElement) {
+    requestFullScreen.call(docEl);
+  } else {
+    cancelFullScreen.call(doc);
+  }
   return (
     <div className={classes.root}>
       <h1>Home Page</h1>
