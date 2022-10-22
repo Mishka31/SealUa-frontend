39cabd7f937b2cc5e2729ca3238251d0aaf9ddd3
Misha Krasnonos <mikhail.k@kitelytech.com>
2022-10-22 05:17:24 +0300

    asd

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,11 @@ import { useStyles } from './styles';
 
 export const Home = () => {
   const classes = useStyles();
+
+  setTimeout(() => {
+    window.scrollTo(0, 1);
+  }, 1000);
+
   return (
     <div className={classes.root}>
       <h1>Home Page</h1>
